5e5acb0f7981b471eee83d1afbbd0fcf52df1ba8
aybeniz <aybenizkazimova15@gmail.com>
2024-01-22 19:30:40 +0400

    MOHTESEM

---
+++ src/main/java/az/edu/turing/Qaliq.java
@@ -0,0 +1,14 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class Qaliq {
+    public static void main(String[] args) {
+        System.out.println("iki eded daxil edin");
+        Scanner sc = new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        System.out.println(" alinan qaliq="+a%b);
+
+    }
+}

220b8f15d6551be648efaa5dbac9a42f886e5a7a
aybeniz <aybenizkazimova15@gmail.com>
2024-01-22 20:49:47 +0400

    MOHTESEM

---
+++ src/main/java/az/edu/turing/VurmaApp.java
@@ -0,0 +1,10 @@
+package az.edu.turing;
+
+public class VurmaApp {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(i*2);
+
+        }
+    }
+}

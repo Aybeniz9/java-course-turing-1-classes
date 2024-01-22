dbc895ab5b40b1b5e71f6416421386490a95f99c
aybeniz <aybenizkazimova15@gmail.com>
2024-01-22 19:35:10 +0400

    MOHTESEM

--- App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        a= s.length*2;
-        System.out.println(a);
-    }
-}

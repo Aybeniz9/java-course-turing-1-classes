054f98cd378e17337deddec878eb06322af60d0d
aybeniz <aybenizkazimova15@gmail.com>
2024-01-22 20:14:32 +0400

    MOHTESEM

--- src/main/java/az/edu/turing/Appii.java
+++ src/main/java/az/edu/turing/Appii.java
@@ -4,41 +4,34 @@ import java.util.Scanner;
 
 public class Appii {
     public static void main(String[] args) {
-        Scanner sc= new Scanner (System.in);
-       int a=sc.nextInt();
-       if(a==1){
-           System.out.println("Yanvar");
-       }
-        else if(a==2){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 1) {
+            System.out.println("Yanvar");
+        } else if (a == 2) {
             System.out.println("Fevral");
-        }
-        else if(a==3){
+        } else if (a == 3) {
             System.out.println("MArt");
-        }else if(a==4){
+        } else if (a == 4) {
             System.out.println("Aprel");
-        }else if(a==5){
+        } else if (a == 5) {
             System.out.println("MAy");
-        }else if(a==6){
+        } else if (a == 6) {
             System.out.println("Iyun");
-        }
-        else if(a==7){
+        } else if (a == 7) {
             System.out.println("Iyul");
-        }else if(a==8){
+        } else if (a == 8) {
             System.out.println("Avqust");
-        }else if(a==9){
+        } else if (a == 9) {
             System.out.println("Snetyabr");
-        }else if(a==10){
-            System.out.println("OKtyabr");}
-    else if(a==11){
-        System.out.println("Noyabr");
-    }else if(a==12) {
-        System.out.println("Dekabr");}
-    else
-           System.out.println("sehv daxil etmisiniz");
-
-
-
-
+        } else if (a == 10) {
+            System.out.println("OKtyabr");
+        } else if (a == 11) {
+            System.out.println("Noyabr");
+        } else if (a == 12) {
+            System.out.println("Dekabr");
+        } else
+            System.out.println("sehv daxil etmisiniz");
 
 
     }

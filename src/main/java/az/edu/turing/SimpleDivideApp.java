e7ecf6d64b961e98a9638a4a129a3589de923a39
aybeniz <aybenizkazimova15@gmail.com>
2024-01-24 19:18:07 +0400

    MOHTESEM

--- src/main/java/az/edu/turing/SimpleDivideApp.java
+++ src/main/java/az/edu/turing/SimpleDivideApp.java
@@ -2,7 +2,7 @@ package az.edu.turing;
 
 import java.util.Scanner;
 
-public class SimpleBolenApp {
+public class SimpleDivideApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();

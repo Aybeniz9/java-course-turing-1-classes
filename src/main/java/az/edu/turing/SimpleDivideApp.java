46e77900fbd3200aad211420fb61b9e5e0734d15
aybeniz <aybenizkazimova15@gmail.com>
2024-01-24 19:22:15 +0400

    MOHTESEM

--- src/main/java/az/edu/turing/SimpleDivideApp.java
+++ src/main/java/az/edu/turing/SimpleDivideApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SimpleDivideApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        final int number = sc.nextInt();
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
                 System.out.print(i + " ");

c96e9ebc73b752c83a5e3fcc3e19f091f6012abe
aybeniz <aybenizkazimova15@gmail.com>
2024-01-26 20:12:40 +0400

    MOHTESEM

--- src/main/java/az/edu/turing/Arrayx.java
+++ src/main/java/az/edu/turing/Arrayx.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Arrayx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         int arr[][] = new int[n][n];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
